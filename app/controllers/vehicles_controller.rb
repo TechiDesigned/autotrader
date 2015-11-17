84e757d23401d8e57b50216ccf61a200bfb8be53
Ski Blanchard <ski.blanchard@careerbuilder.com>
2015-11-16 19:36:47 -0500

    Added users to vehicles

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -16,7 +16,8 @@ class VehiclesController < ApplicationController
 
   # GET /vehicles/new
   def new
-    @vehicle = Vehicle.new
+    @vehicle = current_user.vehicles.new
+
   end
 
   # GET /vehicles/1/edit
@@ -26,7 +27,7 @@ class VehiclesController < ApplicationController
   # POST /vehicles
   # POST /vehicles.json
   def create
-    @vehicle = Vehicle.new(vehicle_params)
+    @vehicle = current_user.vehicles.new(vehicle_params)
 
     respond_to do |format|
       if @vehicle.save
@@ -75,7 +76,8 @@ class VehiclesController < ApplicationController
                                       :color, 
                                       :condition, 
                                       :price, 
-                                      :feature_id, 
+                                      :feature_id,
+                                      :user_id, 
                                       { feature_ids: [] })
     end
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,9 +1,8 @@
 class Vehicle < ActiveRecord::Base
 	
 	belongs_to :category
+	belongs_to :user
 	
 	has_many :vehicle_features
-
 	has_many :features, through: :vehicle_features
-
 end
